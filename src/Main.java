7cdc4a3003c9d4f84be07064c67953a5ea69d1ad
JulyThe9 <arturgumenuk@gmail.com>
2018-02-05 14:52:27 +0100

    Delete Main.java

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Arturs Gumenuks on 25.01.2018.
- */
-public class Main {
-    // this is a test file which should appear on GitHub!
-    // line added!
-    // this time I will pull
-}
